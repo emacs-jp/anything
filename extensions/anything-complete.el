fbe9a7e234d5b849a763e4168059404fcf434176
rubikitch <rubikitch@ruby-lang.org>
2015-04-01 06:44:05 +0900

    * extensions/anything-complete.el: anything-lisp-complete-symbol-partial-match: make arg optional

--- extensions/anything-complete.el
+++ extensions/anything-complete.el
@@ -53,7 +53,7 @@
 ;;    *Whether to sort completion candidates.
 ;;    default = nil
 ;;  `anything-execute-extended-command-use-kyr'
-;;    *Use `anything-kyr' (context-aware commands) in `anything-execute-extended-command'. 
+;;    *Use `anything-kyr' (context-aware commands) in `anything-execute-extended-command'.
 ;;    default = t
 
 ;; * `anything-lisp-complete-symbol', `anything-lisp-complete-symbol-partial-match':
@@ -75,7 +75,7 @@
 ;; (add-to-list 'load-path (expand-file-name "~/elisp"))
 ;;
 ;; Then install dependencies.
-;; 
+;;
 ;; Install anything-match-plugin.el (must).
 ;; M-x install-elisp http://www.emacswiki.org/cgi-bin/wiki/download/anything-match-plugin.el
 ;;
@@ -228,12 +228,12 @@ used by `anything-lisp-complete-symbol-set-timer' and `anything-apropos'"
 
 (defcustom anything-complete-sort-candidates nil
   "*Whether to sort completion candidates."
-  :type 'boolean  
+  :type 'boolean
   :group 'anything-complete)
 
 (defcustom anything-execute-extended-command-use-kyr t
   "*Use `anything-kyr' (context-aware commands) in `anything-execute-extended-command'. "
-  :type 'boolean  
+  :type 'boolean
   :group 'anything-complete)
 (defun alcs-sort-maybe (candidates source)
   (if anything-complete-sort-candidates
@@ -459,7 +459,7 @@ used by `anything-lisp-complete-symbol-set-timer' and `anything-apropos'"
   (interactive "P")
   (anything-lisp-complete-symbol-1 update anything-lisp-complete-symbol-sources
                                    (alcs-initial-input nil)))
-(defun anything-lisp-complete-symbol-partial-match (update)
+(defun anything-lisp-complete-symbol-partial-match (&optional update)
   "`lisp-complete-symbol' replacement using `anything' (partial match)."
   (interactive "P")
   (anything-lisp-complete-symbol-1 update anything-lisp-complete-symbol-sources
@@ -699,7 +699,7 @@ It accepts one argument, selected candidate.")
   (defalias 'anything-old-read-variable (symbol-function 'read-variable))
   (defalias 'anything-old-read-command (symbol-function 'read-command))
   (put 'anything-read-string-mode 'orig-read-buffer-function read-buffer-function))
-  
+
 ;; (progn (anything-read-string-mode -1) anything-read-string-mode)
 ;; (progn (anything-read-string-mode 1) anything-read-string-mode)
 ;; (progn (anything-read-string-mode 0) anything-read-string-mode)
@@ -724,7 +724,7 @@ So, (anything-read-string-mode 1) and
   (setq anything-read-string-mode
         (cond ((consp arg) (setq anything-read-string-mode-flags arg)) ; not interactive
               (arg (> (prefix-numeric-value arg) 0))  ; C-u M-x
-              (t   (not anything-read-string-mode)))) ; M-x 
+              (t   (not anything-read-string-mode)))) ; M-x
   (when (eq anything-read-string-mode t)
     (setq anything-read-string-mode anything-read-string-mode-flags))
   (if anything-read-string-mode
@@ -868,7 +868,7 @@ So, (anything-read-string-mode 1) and
 
 (add-hook 'after-init-hook 'alcs-make-candidates)
 
-      
+
 ;;;; unit test
 ;; (install-elisp "http://www.emacswiki.org/cgi-bin/wiki/download/el-expectations.el")
 ;; (install-elisp "http://www.emacswiki.org/cgi-bin/wiki/download/el-mock.el")
